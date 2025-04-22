a757ab5f5e930a06bbfa8a184a3ec504ad02b73d
shanks-2024 <4pa21cs057@pace.edu.in>
2025-04-22 15:48:59 +0530

    Initial commit

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -74,11 +74,15 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('https://dashboard.render.com/d/dpg-d03momadbo4c738htgng-a'),  # this will come from Render
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
